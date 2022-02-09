766e0c1eca66bc2439769083c9a5136576ef5ccf
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2022-02-09 16:25:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+var name_ = ""
+var email = ""
+var pass = ""
+var phone = ""
+
 function registerUser(){
-    alert("Account created")
+    
+    name_ = document.getElementById("nameInp").value
+    email = document.getElementById("mailInp").value
+    pass = document.getElementById("passInp").value
+    phone = document.getElementById("phoneInp").value
+    alert("Hello "+name_)
+
+    // if(){
+
+    // }
+    window.location.href="dashboard.html"
+
 }
 
 function loginUser(){
     alert("Loged in")
+    email = document.getElementById("email_l").value
+    pass = document.getElementById("pass_l").value
+}
+
+function updatePost(){
+    alert("Post Created🙌🪄")
+    window.location.href="dashboard.html"
 }
